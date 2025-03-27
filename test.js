a1738bb855e6475a4bdf88a9ce58170f1b0b1c02
sravanichintha66 <sravanichintha66@gmail.com>
2025-03-27 13:30:43 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,28 @@
+//
+let a=6;
+a=7
+a=2
+const b=7;
+console.log(b);
+console.log(a);
+console.log(3+4);
+//
+
+//
+for(i=0;i<=5;i++){
+    console.log(i)
+}
+//
+
+//
+let n = 5;
+for (let i = 1; i <= n; i++) {
+    let pattern= "* ";
+    console.log(pattern.repeat(i));
+}
+//
+
+
+
+
+
